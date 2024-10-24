c63c43ef3cf587e460da7616d24f4e157f6b7176
syednoor058 <syednoor058@gmail.com>
2024-10-25 01:33:31 +0600

    Testimonials added responsive.

--- client/src/components/testimonials/TestimonialCard.jsx
+++ client/src/components/testimonials/TestimonialCard.jsx
@@ -1,45 +1,40 @@
+/* eslint-disable react/prop-types */
 // import React from 'react'
 import { FaQuoteLeft } from "react-icons/fa";
-import shararImg from "../../assets/images/testimonials/sharar.webp";
 
 import { Avatar } from "@mui/material";
 
-export default function TestimonialCard() {
+export default function TestimonialCard(props) {
   return (
-    <div className="w-full bg-darkPrimary flex flex-col gap-16 rounded-t-3xl shadow-md">
+    <div className="w-full aspect-[9/13] sm:aspect-[9/14] lg:aspect-[9/16] xl:aspect-[9/12] bg-darkPrimary flex flex-col gap-16 rounded-t-3xl shadow-xl relative">
       <div className="p-5 text-lightSecondary flex flex-col gap-4">
         <div className="text-lightPrimary text-4xl">
           <FaQuoteLeft />
         </div>
-        <div>
-          Our business grew faster than we imagined after partnering with this
-          agency. Their strategic guidance and digital solutions boosted our
-          revenue by 50% within months. They don&apos;t just deliver
-          services—they help us unlock potential we didn&apos;t know we had.
-        </div>
+        <div className="text-sm lg:text-base">{props.feedback}</div>
       </div>
-      <div className="bg-lightPrimary relative">
+      <div className="w-full bg-accentColor absolute bottom-0">
         <div className="w-full flex flex-col gap-3 justify-center items-center text-center leading-none -mt-10">
           <div className="p-[7px] bg-darkPrimary rounded-[100%] relative">
             <Avatar
-              alt="Fahim Sharar"
-              src={shararImg}
+              alt={props.client}
+              src={props.image}
               sx={{ width: 90, height: 90 }}
             />
             <div className="w-7 h-7 top-[40px] -left-[27px] bg-darkPrimary absolute">
-              <div className="w-full h-full bg-lightPrimary rounded-tr-[100%]"></div>
+              <div className="w-full h-full bg-accentColor rounded-tr-[100%]"></div>
             </div>
             <div className="w-7 h-7 top-[40px] -right-[27px] bg-darkPrimary absolute">
-              <div className="w-full h-full bg-lightPrimary rounded-tl-[100%]"></div>
+              <div className="w-full h-full bg-accentColor rounded-tl-[100%]"></div>
             </div>
           </div>
           <div className="w-full flex flex-col gap-1 px-5 pb-5 text-darkSecondary leading-none uppercase">
             <div className="text-lg  font-semibold text-darkPrimary uppercase">
-              Kazi Fahim Sharar
+              {props.client}
             </div>
             <div className="flex flex-col gap-[3px]">
-              <div className="">Puppetbrush Digital Solution</div>
-              <div className="text-xs font-light">Cheif Executive Officer</div>
+              <div className="">{props.company}</div>
+              <div className="text-xs font-light">{props.pos}</div>
             </div>
           </div>
         </div>
--- client/src/components/testimonials/Testimonials.jsx
+++ client/src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,83 @@
+/* eslint-disable react/prop-types */
 // import React from "react";
-
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from "react-icons/md";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import azizulImg from "../../assets/images/testimonials/azizul.webp";
+import rituImg from "../../assets/images/testimonials/rhitu.webp";
+import shararImg from "../../assets/images/testimonials/sharar.webp";
+import tanhImg from "../../assets/images/testimonials/tanha.webp";
 import TestimonialCard from "./TestimonialCard";
 
-export default function Testimonials() {
+function SampleNextArrow(props) {
+  const { onClick } = props;
   return (
-    <div className="py-10 lg:py-20 font-bodyFont text-darkSecondary bg-accentColor">
-      <div className="flex flex-col gap-20 px-5 sm:px-7 lg:px-10 xl:px-20">
+    <div
+      className="text-darkPrimary text-5xl absolute top-[50%] -right-10 cursor-pointer hidden xl:flex"
+      onClick={onClick}
+      // data-aos="zoom-in"
+    >
+      <MdOutlineKeyboardArrowRight />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      className="text-darkPrimary text-5xl absolute top-[50%] -left-10 cursor-pointer hidden xl:flex"
+      onClick={onClick}
+      // data-aos="zoom-in"
+    >
+      <MdOutlineKeyboardArrowLeft />
+    </div>
+  );
+}
+
+export default function Testimonials() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    initialSlide: 0,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          nextArrow: <SampleNextArrow />,
+          prevArrow: <SamplePrevArrow />,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="py-10 lg:py-20 font-bodyFont text-darkSecondary bg-accentColor relative overflow-hidden">
+      <div className="w-full h-1/2 bg-lightPrimary absolute bottom-0"></div>
+      <div className="flex flex-col gap-10 xl:gap-20 relative">
         <div className="flex flex-col sm:flex-row gap-5 px-5 sm:px-7 lg:px-10 xl:px-20">
           <div className="w-full sm:w-[30%] flex flex-row gap-3 sm:gap-10">
             <div className="uppercase font-semibold text-sm sm:text-lg text-darkSecondary">
@@ -18,10 +90,46 @@ export default function Testimonials() {
             <span className="text-lightPrimary font-light">Clients</span> Speak
           </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
+
+        <div className="px-5 sm:px-7 lg:px-10 xl:px-20">
+          <Slider {...settings}>
+            <div className="px-2 sm:px-3">
+              <TestimonialCard
+                image={shararImg}
+                feedback="Our business grew faster than we imagined after partnering with this agency. Their strategic guidance and digital solutions boosted our revenue by 50% within months. They don't just deliver services—they help us unlock potential we didn't know we had."
+                client="Kazi Fahim Sharar"
+                company="Puppetbrush Digital Solution"
+                pos="Cheif Executive Officer"
+              />
+            </div>
+            <div className="px-2 sm:px-3">
+              <TestimonialCard
+                image={tanhImg}
+                feedback="With their SEO expertise and digital marketing campaigns, we saw a 200% increase in website traffic within the first quarter. They transformed our online presence and positioned us as an authority in our industry."
+                client="Sheikh Tanjina Tanha"
+                company="Sweet Cakes Food Groceries"
+                pos="Founder & CEO"
+              />
+            </div>
+            <div className="px-2 sm:px-3">
+              <TestimonialCard
+                image={rituImg}
+                feedback="Thanks to their tailored marketing strategies, our customer engagement skyrocketed, and our retention rate improved by 35%. Their team understands our audience perfectly and knows how to keep them engaged."
+                client="Dr. Rhituparna Das"
+                company="Lexerburg Dental Care"
+                pos="Owner"
+              />
+            </div>
+            <div className="px-2 sm:px-3">
+              <TestimonialCard
+                image={azizulImg}
+                feedback="We relied on them for software development, and they exceeded expectations with flawless, reliable solutions. Their rigorous testing ensured a smooth launch with zero downtime—simply outstanding!"
+                client="Md. Azizul Haque"
+                company="InterAid education care"
+                pos="Founder & Head Teacher"
+              />
+            </div>
+          </Slider>
         </div>
       </div>
     </div>
