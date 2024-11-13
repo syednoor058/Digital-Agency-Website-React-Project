1daec49f291a64520b9aa157a9f08c391d308b35
syednoor058 <syednoor058@gmail.com>
2024-11-13 15:53:33 +0600

    Navbar animation added.

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
-import { useState } from "react";
+import gsap from "gsap";
+import { useLayoutEffect, useRef, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { NavLink } from "react-router-dom";
 import lightLogo from "../../assets/logo/dark_mode_logo.png";
@@ -8,96 +9,158 @@ export default function Navbar() {
   const toggleMenu = () => {
     setToggle(!toggle);
   };
+
+  const comp = useRef(null);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const navTimeline = gsap.timeline();
+      navTimeline
+        .from(["#navLogo", "#mobile-nav0", "#mobile-nav1", "#mobile-nav2"], {
+          yPercent: "-=30",
+          opacity: 0,
+          delay: 4.1,
+          duration: 0.4,
+          ease: "expo.inOut",
+          stagger: 0.1,
+        })
+        .from(
+          ["#nav0", "#nav1", "#nav2", "#nav3", "#nav4", "#nav5"],
+          {
+            y: "-=30",
+            opacity: 0,
+            duration: 0.3,
+            ease: "expo.inOut",
+            stagger: 0.1,
+          },
+          "<0.3"
+        )
+        .from(
+          "#nav-cta",
+          {
+            yPercent: "-=30",
+            opacity: 0,
+            duration: 0.3,
+            ease: "expo.inOut",
+          },
+          "<0.7"
+        );
+    }, comp);
+
+    return () => ctx.revert();
+  }, []);
   return (
-    <div className="w-full font-titleFont capitalize text-lightBodyText bg-darkPrimary">
+    <div
+      ref={comp}
+      className="w-full font-titleFont capitalize text-lightBodyText bg-darkPrimary"
+    >
       <div className="w-full h-16 px-3 md:px-8 lg:px-8 xl:px-20 hidden lg:flex flex-row gap-10 items-center justify-between">
-        <div className="w-[15%]">
+        <div id="navLogo" className="w-[15%]">
           <img className="w-full h-auto" src={lightLogo} alt="light_logo" />
         </div>
         <div className="w-full flex flex-row gap-20 justify-end items-center">
           <div className="flex flex-row gap-5 justify-center items-center text-base lg:text-sm">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? " duration-300 group text-accentColor overflow-hidden"
-                  : "flex flex-col justify-center items-center overflow-hidden group"
-              }
-            >
-              Home
-              <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
-            </NavLink>
-            <NavLink
-              to="/services"
-              className={({ isActive }) =>
-                isActive
-                  ? " duration-300 group text-accentColor overflow-hidden"
-                  : "flex flex-col justify-center items-center overflow-hidden group"
-              }
-            >
-              Services
-              <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
-            </NavLink>
-            <NavLink
-              to="/projects"
-              className={({ isActive }) =>
-                isActive
-                  ? " duration-300 group text-accentColor overflow-hidden"
-                  : "flex flex-col justify-center items-center overflow-hidden group"
-              }
-            >
-              Projects
-              <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
-            </NavLink>
-            <NavLink
-              to="/pricing"
-              className={({ isActive }) =>
-                isActive
-                  ? " duration-300 group text-accentColor overflow-hidden"
-                  : "flex flex-col justify-center items-center overflow-hidden group"
-              }
-            >
-              Pricing
-              <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
-            </NavLink>
-            <NavLink
-              to="/about-us"
-              className={({ isActive }) =>
-                isActive
-                  ? " duration-300 group text-accentColor overflow-hidden"
-                  : "flex flex-col justify-center items-center overflow-hidden group"
-              }
-            >
-              About
-              <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
-            </NavLink>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) =>
-                isActive
-                  ? " duration-300 group text-accentColor overflow-hidden"
-                  : "flex flex-col justify-center items-center overflow-hidden group"
-              }
-            >
-              Contact
-              <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
-            </NavLink>
+            <div id="nav0" className="overflow-hidden">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? " duration-300 group text-accentColor overflow-hidden"
+                    : "flex flex-col justify-center items-center overflow-hidden group"
+                }
+              >
+                Home
+                <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
+              </NavLink>
+            </div>
+
+            <div className="overflow-hidden" id="nav1">
+              <NavLink
+                to="/services"
+                className={({ isActive }) =>
+                  isActive
+                    ? "flex flex-col justify-center items-center duration-300 group text-accentColor overflow-x-hidden"
+                    : "flex flex-col justify-center items-center overflow-hidden group"
+                }
+              >
+                Services
+                <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
+              </NavLink>
+            </div>
+            <div className="overflow-hidden" id="nav2">
+              <NavLink
+                to="/projects"
+                className={({ isActive }) =>
+                  isActive
+                    ? " duration-300 group text-accentColor overflow-hidden"
+                    : "flex flex-col justify-center items-center overflow-hidden group"
+                }
+              >
+                Projects
+                <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
+              </NavLink>
+            </div>
+            <div className="overflow-hidden" id="nav3">
+              <NavLink
+                to="/pricing"
+                className={({ isActive }) =>
+                  isActive
+                    ? " duration-300 group text-accentColor overflow-hidden"
+                    : "flex flex-col justify-center items-center overflow-hidden group"
+                }
+              >
+                Pricing
+                <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
+              </NavLink>
+            </div>
+            <div className="overflow-hidden" id="nav4">
+              <NavLink
+                to="/about-us"
+                className={({ isActive }) =>
+                  isActive
+                    ? " duration-300 group text-accentColor overflow-hidden"
+                    : "flex flex-col justify-center items-center overflow-hidden group"
+                }
+              >
+                About
+                <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
+              </NavLink>
+            </div>
+            <div className="overflow-hidden" id="nav5">
+              <NavLink
+                to="/contact-us"
+                className={({ isActive }) =>
+                  isActive
+                    ? " duration-300 group text-accentColor overflow-hidden"
+                    : "flex flex-col justify-center items-center overflow-hidden group"
+                }
+              >
+                Contact
+                <div className="group-hover:translate-x-0 -translate-x-[105%] h-[1px] w-full bg-accentColor duration-[400ms]"></div>
+              </NavLink>
+            </div>
           </div>
-          <div className="w-[15%] px-3 py-2 border border-accentColor rounded-full flex justify-center items-center lg:text-sm">
+          <div
+            id="nav-cta"
+            className="w-[15%] px-3 py-2 border border-accentColor rounded-full flex justify-center items-center lg:text-sm"
+          >
             Free trial
           </div>
         </div>
       </div>
       <div className="w-full h-14 px-3 md:px-8 lg:px-8 xl:px-20 text-xs sm:text-sm  flex lg:hidden items-center">
         <div className="w-full flex flex-row gap-10 items-center justify-between  text-lightTitleText">
-          <div className="">
+          <div className="" id="mobile-nav0">
             <img className="w-auto h-14" src={lightLogo} alt="light_logo" />
           </div>
           <div className="flex flex-row gap-2 sm:gap-3 justify-end items-center">
-            <div className="px-3 py-2 border border-accentColor rounded-full flex justify-center items-center ">
+            <div
+              id="mobile-nav1"
+              className="px-3 py-2 border border-accentColor rounded-full flex justify-center items-center "
+            >
               Free trial
             </div>
             <div
+              id="mobile-nav2"
               className="p-2 rounded-full cursor-pointer text-lg"
               onClick={toggleMenu}
             >
